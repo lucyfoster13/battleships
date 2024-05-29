2f9927c6dafbda731560b1cdf6a4f12e008c2bcc
Lucy Foster <lucyfoster13@gmail.com>
2024-05-29 14:05:02 +0000

    Create gameboard of 8*8

--- run.py
+++ run.py
@@ -3,6 +3,18 @@ HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
 # Board for displaying hits and misses
 GUESS_BOARD = [[" "] * 8 for i in range(8)]
 
+"""
+Create a board of 8 rows and 8 columns
+"""
+def print_board(board):
+    print("  A B C D E F G H")
+    print("  +-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        #%d for decimal, %s for string
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
 letters_to_numbers = {
     'A': 0,
     'B': 1,
@@ -26,3 +38,6 @@ def get_ship_location():
 def count_hit_ships():
     pass
 
+create_ships()
+turns = 10
+while turns > 0:
